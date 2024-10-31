83acf22c408cf027768c81cb3f2ac03064ea0134
yusuke nakajima <yuske@developer-nakajima.xyz>
2024-11-01 02:37:51 +0900

    ver1: LPボタン押下時の処理を移植

--- src/lib/ver1/_4-uiSetup.js
+++ src/lib/ver1/_4-uiSetup.js
@@ -1,10 +1,10 @@
 async function trUiSetup() {
   await trLpSetup((i) => {
-    setDataGridIsPressed(i, !trGetPressedKeyList(TR_DATA_GRID).includes(i))
+    trSetDataGridIsPressed(i, !trGetPressedKeyList(TR_DATA_GRID).includes(i))
 
     // 背景色を変更する
-    trCalcDataGridResult = calcDataGrid(dataGrid)
-    const _color = _colorArray.at(trCalcDataGridResult.key0 % TR_COLOR_ARRAY.length)
+    trCalcDataGridResult = trCalcDataGrid(TR_DATA_GRID)
+    const _color = TR_COLOR_ARRAY.at(trCalcDataGridResult.key0 % TR_COLOR_ARRAY.length)
     if (!_color) {
       return
     }
--- src/lib/ver1/_5-uiDraw.js
+++ src/lib/ver1/_5-uiDraw.js
@@ -1,2 +1,12 @@
 function trUiDraw() {
+  background(color._1)
+
+  // fill - color2
+  drawShape(ceil(trCalcDataGridResult.key1), 20, 0.85, color._2, true)
+
+  // fill - color3
+  drawShape(ceil(trCalcDataGridResult.key2), 20, 1.5, color._3, false)
+
+  // fill - color3
+  drawShape(ceil(trCalcDataGridResult.key3), 20, 0.3, color._3, true)
 }
