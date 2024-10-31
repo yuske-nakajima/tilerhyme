8f74caaa5618a1ad81d10bf6fd6c9d8f0f741c90
yusuke nakajima <yuske@developer-nakajima.xyz>
2024-11-01 01:18:36 +0900

    var1: 関数・定数・変数を移植

--- src/lib/_1-helper.js
+++ src/lib/_1-helper.js
@@ -1,6 +1,6 @@
 /**
  * 他描画に影響範囲が及ばないようにpushとpopで囲む
- * @param {function} 描画関数
+ * @param {function} func - 描画関数
  */
 function trDrawBlock(func) {
   push()
@@ -8,13 +8,27 @@ function trDrawBlock(func) {
   pop()
 }
 
+/**
+ * @param {Object} pos - 位置オブジェクト
+ * @param {number} pos.x - x座標
+ * @param {number} pos.y - y座標
+ * @param {Object} size - サイズオブジェクト
+ * @param {number} size.width - 幅
+ * @param {number} size.height - 高さ
+ * @param {function} func - 実行する関数
+ */
 function trMousePressedBlock(pos, size, func) {
   if (pos.x <= mouseX && mouseX <= pos.x + size.width && pos.y <= mouseY && mouseY <= pos.y + size.height) {
     func()
   }
 }
 
-// ローカルストレージから値を取得または初期化する関数
+/**
+ * ローカルストレージから値を取得または初期化する関数
+ * @param {string} key - ローカルストレージのキー
+ * @param {*} defaultValue - デフォルト値
+ * @returns {*} - 取得した値またはデフォルト値
+ */
 function trGetOrInitializeValue(key, defaultValue) {
   const storedValue = localStorage.getItem(key)
   if (storedValue !== null) {
@@ -24,7 +38,12 @@ function trGetOrInitializeValue(key, defaultValue) {
   return defaultValue
 }
 
-// ローカルストレージに値を保存する関数
+/**
+ * ローカルストレージに値を保存する関数
+ * @param {string} key - ローカルストレージのキー
+ * @param {*} value - 保存する値
+ * @returns {*} - 保存した値
+ */
 function trSaveToLocalStorage(key, value) {
   const existingValue = localStorage.getItem(key)
   if (existingValue !== null) {
@@ -32,4 +51,107 @@ function trSaveToLocalStorage(key, value) {
   }
   localStorage.setItem(key, JSON.stringify(value))
   return value
+}
+
+/**
+ * LounchPadのボタンが押されたかどうかを判定する関数
+ * @param {number} data - ボタンのデータ
+ * @returns {boolean} - 押されたかどうか
+ */
+function trLpIsPressed(data) {
+  return data === 127
+}
+
+/**
+ * @param {Array.<{isPressed: boolean, value: number}>} dataGrid - キーのデータグリッド
+ * @returns {number[]} - 押されたキーのリスト
+ */
+function trGetPressedKeyList(dataGrid) {
+  const result = []
+  for (const item of dataGrid) {
+    if (item.isPressed) {
+      result.push(item.value)
+    }
+  }
+  return result
+}
+
+/**
+ * [Launchpad Mini MK3]のセットアップを行う関数
+ * @param {function(number): void} pressedCallback - ボタンが押されたときのコールバック関数
+ * @param {Array.<{isPressed: boolean, value: number}>} dataGrid - キーのデータグリッド
+ * @returns {Promise<string|undefined>} - エラーメッセージまたはundefined
+ */
+async function trLpSetup(pressedCallback, dataGrid) {
+  try {
+    const access = await navigator.requestMIDIAccess()
+
+    const input = Array.from(access.inputs.values())
+      .filter((input) => {
+        return (
+          input.name === 'Launchpad Mini MK3 LPMiniMK3 MIDI Out' || // mac
+          input.name === 'MIDIIN2 (LPMiniMK3 MIDI)' // windows
+        )
+      })
+      .at(0)
+    if (!input) {
+      const errorMessage = 'inputが見つかりません'
+      console.error(errorMessage)
+      return errorMessage
+    }
+
+    const output = Array.from(access.outputs.values())
+      .filter((output) => {
+        return (
+          output.name === 'Launchpad Mini MK3 LPMiniMK3 MIDI In' || // mac
+          output.name === 'MIDIOUT2 (LPMiniMK3 MIDI)' // windows
+        )
+      })
+      .at(0)
+    if (!output) {
+      const errorMessage = 'outputが見つかりません'
+      console.error(errorMessage)
+      return errorMessage
+    }
+
+    for (let i = 36; i <= 99; i++) {
+      output.send([0x90, i, 0])
+    }
+
+    input.onmidimessage = function (event) {
+      const status = event.data[0]
+      const note = event.data[1]
+      const velocity = event.data[2]
+
+      // 有効なデータのみ処理する
+      if (!status || !note || !velocity) {
+        return
+      }
+
+      for (let i = 36; i <= 99; i++) {
+        if (note !== i) {
+          continue
+        }
+
+        if (trLpIsPressed(velocity)) {
+          pressedCallback(note)
+        }
+
+        if (trGetPressedKeyList(dataGrid).includes(i)) {
+          output.send([0x90, i, 41 /* 色コード */])
+        } else {
+          output.send([0x90, i, 0])
+        }
+      }
+    }
+  } catch (e) {
+    const errorMessage = `MIDIデバイスの接続に失敗しました: ${e}`
+    console.error(errorMessage)
+
+    // TODO: MIDIデバイスが接続されていないときの処理
+    // 画面にデバイスに模したUIを表示する
+    // 画面上でボタンを押すと、それを押したことにする
+    // const note = 処理
+    // pressedCallback(note);
+  }
 }
