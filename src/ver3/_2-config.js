bf3c7f010d09f14336ce601a2efb89c98d37ae71
yusuke nakajima <yuske@developer-nakajima.xyz>
2025-01-31 00:33:30 +0900

    ver-3を整理

--- src/ver3/_2-config.js
+++ src/ver3/_2-config.js
@@ -2,7 +2,7 @@
 // --- 定数
 // ------------------------------------------------------------
 const TR_APP_NAME = 'TILERHYME'
-const TR_VERSION = '2.2'
+const TR_VERSION = '3'
 const TR_VERSION_NAME = 'KICK'
 
 const TR_FUNCTION_CODE = {
